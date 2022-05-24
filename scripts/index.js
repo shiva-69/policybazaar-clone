34b0da839efb07367c342904a31ac36ef946aae1
Shivam Pandey <92380211+shiva-69@users.noreply.github.com>
2022-05-24 16:06:34 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,1 @@
-document.querySelector("#show-login").addEventListener("click",function(){
-    document.querySelector(".popup").classList.add("active")
-        })
-    
-        document.querySelector(".popup .close-btn").addEventListener("click",function(){
-    document.querySelector(".popup").classList.remove("active")
-        })
+
