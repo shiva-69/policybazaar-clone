0e6ce8c749e0dbf6100c08e63a9c25d3ab6887ca
shiva-69 <92380211+shiva-69@users.noreply.github.com>
2022-05-26 00:18:36 +0530

    sign in page functionality done

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
 
+const setClass = () => {
+    let modal = document.getElementById("modal");
+    modal.setAttribute("class", "active");
+    let popup = document.getElementById("popup");
+    popup.setAttribute("class", "active");
+    console.log("clicked")
+}
+document.getElementById("signInButton").addEventListener("click", setClass);
+document.getElementById("prdCard1").addEventListener("click", setClass);
+document.getElementById("closesignin").addEventListener("click", ()=>{
+    let modal = document.getElementById("modal");
+    modal.classList.remove("active");
+    let popup = document.getElementById("popup");
+    popup.classList.remove("active");
+    console.log("clicked")
+}) 
