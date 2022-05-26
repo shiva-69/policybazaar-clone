0aca9d52fe53c2b2272d2a2af7e62bc8c33f465c
Avi1702 <96744787+Avi1702@users.noreply.github.com>
2022-05-26 22:16:23 +0530

    mohit page edited

---
+++ scripts/health_insurance.js
@@ -0,0 +1,27 @@
+
+function male(){
+
+    // console.log("hi")
+    let male_box=document.querySelector(".male")
+  
+    if(male_box.style.backgroundColor==="white"){
+        male_box.style.backgroundColor="rgb(184, 230, 184)"
+        console.log("hi")
+    }
+    else{
+        male_box.style.backgroundColor="white"
+    }
+}
+function female(){
+
+    // console.log("hi")
+    let male_box=document.querySelector(".female")
+  
+    if(male_box.style.backgroundColor==="white"){
+        male_box.style.backgroundColor="rgb(184, 230, 184)"
+        console.log("hi")
+    }
+    else{
+        male_box.style.backgroundColor="white"
+    }
+}
