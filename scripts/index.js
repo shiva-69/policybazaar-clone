df34f99b9cd7294194f27d06c087f531ba0c794a
gauravsft <gauravsft@gmail.com>
2022-05-26 13:52:27 +0530

    main page link file

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
+document.querySelector(".card8").addEventListener("click",function(){
+    window.location.href="../saralbima.html";
+})
+
+document.getElementById("renew1").addEventListener("click",function(){
+    window.location.href="../insurenceRenewal.html";
+})
 
